e52125c3f609c05ae46441f9204a39b06073638d
David16021 <david16021@gmail.com>
2021-08-20 02:47:11 -0500

    Cambiamos el comportamiendo por defecto de un evento

--- javascript/main.js
+++ javascript/main.js
@@ -6,7 +6,9 @@ let links = document.querySelectorAll(".close");
 
 links.forEach(function (link) {
   //Agregar un evento click a cada uno de ellos
-  link.addEventListener("click", function () {
-    console.log(":)");
+  link.addEventListener("click", function (ev) {
+    ev.preventDefault();
+
+    return false;
   });
 });
