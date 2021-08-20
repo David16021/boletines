bc4b845951e3499390dd3ef63fa0d4f80a224c21
David16021 <david16021@gmail.com>
2021-08-20 02:43:44 -0500

    Observamos algunos eventos usando javascript

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,12 @@
-//DOM
-// querySelector //querySelectorAll
-let links = document.querySelectorAll("a");
+//obtener los elementos de la clase .close
+
+let links = document.querySelectorAll(".close");
+
+//Recorrerlos
 
 links.forEach(function (link) {
-  console.log(link);
+  //Agregar un evento click a cada uno de ellos
+  link.addEventListener("click", function () {
+    console.log(":)");
+  });
 });
